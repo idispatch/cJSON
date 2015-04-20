bde0af64a483871cda81141e6a73464e488e4150
Oleg Kosenkov <oleg@kosenkov.ca>
2015-04-19 20:49:05 -0400

    Added x86 variant

--- common.mk
+++ common.mk
@@ -7,8 +7,15 @@ QCONFIG=qconfig.mk
 endif
 include $(QCONFIG)
 
+#===== USEFILE - the file containing the usage message for the application.
 USEFILE=
+
+#===== NAME - name of the project (default - name of project directory).
 NAME=cJSON
+
+#===== EXTRA_SILENT_VARIANTS - variants that are not appended to the result binary name (like MyBin_g)
+EXTRA_SILENT_VARIANTS+=x86
+
 include $(MKFILES_ROOT)/qmacros.mk
 ifndef QNX_INTERNAL
 QNX_INTERNAL=$(PROJECT_ROOT)/.qnx_internal.mk
@@ -16,5 +23,7 @@ endif
 include $(QNX_INTERNAL)
 
 include $(MKFILES_ROOT)/qtargets.mk
+
 OPTIMIZE_TYPE_g=none
 OPTIMIZE_TYPE=$(OPTIMIZE_TYPE_$(filter g, $(VARIANTS)))
+
